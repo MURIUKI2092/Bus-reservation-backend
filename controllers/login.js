8c820eeb42a0e2e4e0ce2128ce1934446e6fea41
MURIUKI2092 <muriukijames33@gmail.com>
2022-03-22 17:09:35 +0300

    #fixed login api without integrating a database

--- controllers/login.js
+++ controllers/login.js
@@ -4,49 +4,27 @@ const bcrypt = require('bcrypt')
 
 
 
-const Login=(req,res)=>{
-  const {email,password}=req.body
-  //finds the first occurrence of user in the database
-  User.findOne({email:req.body.email})
-  // then assigns  the User a user
-  .then(user =>{
-    // if no username or password given it returns a status code and an error message
-    if(!email || !password){
-      return res.statusCode (400).json({
-        message:"Username or Password not present"
-      })
+app.post ('/users/login',(req,res)=>{
+  bcrypt.hash(req.body.password,10,(err,hash)=>{
+    if (err){
+      return res.status(500).json({
+        error:err
+      });
+
+    }else{
+      const user= {
+        email:req.body.email,
+        password:hash
+
+      }
+      res.status(201).json({
+        message:"User has been logged in successfully"
+      });
+      
     }
-        // if no user it returns a status code and an error message
-    if(!user){
-      res.statusCode(404).json({
-        error:' The user with that email was not found'})
-
-    }
-    // if user present it hashes the typed password and compare with the stored 
-    // password in the database
-    else{
-      bcrypt.compare(req.body.password,user.password,(error,result)=>{
-        if(error){
-          //if error occurs during hashing it returns the error
-          res.statusCode(500).json(error)
-        }
-        else if(result){
-          //  if match then generate token for the user
-          res.statusCode(200).json({
-            message:"Login successful"});
-
-
-        }
-        else{
-          res.status(403).json({
-            error:"password do not match"})
-        }
-
-      })
-
-    }
+    
   })
 
 
-}
+})
 module.exports = Login();
--- server.js
+++ server.js
@@ -14,16 +14,5 @@ app.get ('/users',(req,res)=>{
   res.json(users)
 });
 
-app.post ('/users/login',(req,res)=>{
-  bcrypt.hash(req.body.password,10,(err,hash)=>{
-    if (err){
-      return res.status(500).json({
-        error:err
-      });
 
-    }
-  })
-
-
-})
 app.listen(5000)
